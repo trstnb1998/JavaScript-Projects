342bdd17f89f886c7988c872e31c41397c4c7d5e
Tristan <tristanjaybartolome@gmail.com>
2025-03-05 21:52:49 +1100

    config variable

--- Flappy_Bird/app.js
+++ Flappy_Bird/app.js
@@ -1,3 +1,23 @@
+let config = {
+    renderer: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: { y: 300},
+            debugv: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+let game = new Phaser.Game(config);
+
 function preload() {
 
 }
