1523147638cf88231dd49126a30322cb936d871e
Tristan <tristanjaybartolome@gmail.com>
2025-03-20 21:08:41 +1100

    add fetchWeather function

---
+++ weather-app/script.js
@@ -0,0 +1,6 @@
+function fetchWeather() {
+    let searchInput = document.getElementById("search").value;
+    const weatherDataSection = document.getElementById("weather-data");
+    weatherDataSection.style.display = "block";
+    const apiKey = WEATHER_API_KEY;
+}
