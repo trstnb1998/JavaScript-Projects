2e62ae3a80e3e9c32ee35a7f8c514f179f678922
Tristan <tristanjaybartolome@gmail.com>
2025-03-24 14:46:06 +1100

    add conditional message for empty input

--- weather-app/script.js
+++ weather-app/script.js
@@ -3,4 +3,14 @@ function fetchWeather() {
     const weatherDataSection = document.getElementById("weather-data");
     weatherDataSection.style.display = "block";
     const apiKey = WEATHER_API_KEY;
+
+    if (searchInput == "") {
+        weatherDataSection.innerHTML = `
+        <div>
+            <h2>Empty Input!</h2>
+            <p>Please try again with a valid <u>city name</u>.</p>
+        </div>
+        `;
+        return
+    }
 }
