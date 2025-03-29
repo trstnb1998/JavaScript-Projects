cbd7f0f57b66a12ec57f92df273c7ccbca5715a6
Tristan <tristanjaybartolome@gmail.com>
2025-03-29 21:35:04 +1100

    move data image and indent fix

--- weather-app/script.js
+++ weather-app/script.js
@@ -6,10 +6,10 @@ function fetchWeather() {
 
     if (searchInput == "") {
         weatherDataSection.innerHTML = `
-        <div>
-            <h2>Empty Input!</h2>
-            <p>Please try again with a valid <u>city name</u>.</p>
-        </div>
+            <div>
+                <h2>Empty Input!</h2>
+                <p>Please try again with a valid <u>city name</u>.</p>
+            </div>
         `;
         return
     }
@@ -17,17 +17,16 @@ function fetchWeather() {
     async function getLonAndLat() {
         const countryCode = 1;
         const geocodeURL = `https://api.openweathermap.org/geo/1.0/direct?q=${searchInput.replace(" ", "%20")},${countryCode}&limit=1&appid=${apiKey}`
+
         if (!response.ok) {
             console.log("Bad response! ", response.status);
             return
         }
 
         const data = await response.json();
-
         if (data.length == 0) {
             console.log("Something went wrong here.");
             weatherDataSection.innerHTML = `
-            <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="${data.weather[0].description}" width="100" />
                 <div>
                     <h2>Invalid Input: "${searchInput}"</h2>
                     <p>Please try again with a valid <u>city name</u>.</p>
@@ -42,11 +41,16 @@ function fetchWeather() {
     async function getWeatherData() {
         const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
         const response = await fetch(weatherURL);
+
         if (!response.ok) {
             console.log("Bad response! ", response.status);
             return;
         }
 
         const data = await response.json();
+
+        weatherDataSection.innerHTML = `
+            <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="${data.weather[0].description}" width="100" /> 
+        `
     }
 }
